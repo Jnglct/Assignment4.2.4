a9b491a48c7870c91bb1f03a9b1104b233f534e6
Jnglct <rogerscole99@gmail.com>
2025-08-05 16:29:28 -0500

    Add project files.

---
+++ Assignment4.2.4/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Labs
+{
+    public static class Palindrome
+    {
+        public static bool IsPalindrom(string UserString) 
+        {
+            int L = 0;
+            int R = UserString.Length;
+            --R;
+
+                while( L < R)
+                {
+                    if (UserString[L] != UserString[R])
+                    {
+                        return false;
+                    }
+                    ++L;
+                        --R;
+                }
+                return true;
+        }
+
+    }
+}
---
+++ Assignment4.2.4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Assignment4._2._4
+{
+    internal class Program
+    {
+                public static void IsPalindrom(string UserString) 
+        {
+            UserString = UserString.ToLower();
+            int L = 0;
+            int R = UserString.Length - 1;
+            CheckIfPalindrome(UserString,L,R);
+        }
+        static void CheckIfPalindrome(string UserString, int L, int R)
+        {
+            if (UserString[L] != UserString[R])
+            {
+                Console.WriteLine("The string is not Palindrome");
+
+            }
+            else if (L > R)
+            {
+                Console.WriteLine("The string is Palindrome");
+            }
+            else
+            {
+                ++L;
+                --R;
+                CheckIfPalindrome(UserString, L, R);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check if it is a Palindrome");
+            IsPalindrom(Console.ReadLine());
+        }
+    }
+}
